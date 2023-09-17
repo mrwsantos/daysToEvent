d66e726e8242704d721a49d11730d40c785baf7f
David <davidsilva.sp@outlook.com>
2023-09-16 22:13:11 -0300

    forecast

--- src/pages/components/Counter.tsx
+++ src/pages/components/Counter.tsx
@@ -22,7 +22,7 @@ const Counter = () => {
 
     if (difference > 0) {
       timeLeft = {
-        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)) - 1,
         hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
         minutes: Math.floor((difference / 1000 / 60) % 60),
         seconds: Math.floor((difference / 1000) % 60),
---
+++ src/pages/components/Forecast.tsx
@@ -0,0 +1,157 @@
+import { useState, useEffect, useContext } from "react";
+import axios from "axios";
+
+import {
+  CloudLightning,
+  CloudRain,
+  CloudSnow,
+  CloudSun,
+  Snowflake,
+  Sun,
+  Wind,
+} from "@phosphor-icons/react";
+import DataContext from "../../context/DataContext";
+
+import styles from "../../styles/components/Forecast.module.scss";
+import moment from "moment";
+
+function getIcon(weatherId: string) {
+  if (/^2[0-9]/g.test(weatherId)) {
+    return <CloudLightning size={32} />;
+  }
+
+  if (/^3[0-9]/g.test(weatherId)) {
+    return <CloudRain size={32} />;
+  }
+
+  if (/^5[0-9]/g.test(weatherId)) {
+    return <CloudSnow size={32} />;
+  }
+
+  if (/^6[0-9]/g.test(weatherId)) {
+    return <Snowflake size={32} />;
+  }
+
+  if (/^7[0-9]/g.test(weatherId)) {
+    return <Wind size={32} />;
+  }
+
+  if (/^8[0-9]/g.test(weatherId)) {
+    return <Sun size={32} />;
+  }
+
+  if (/^80[0-9]/g.test(weatherId)) {
+    return <CloudSun size={32} />;
+  }
+
+  return null;
+}
+
+const Forecast = () => {
+  const { eventDate } = useContext(DataContext);
+  const [forecast, setForecast] = useState({} as any);
+
+  useEffect(() => {
+    const baseURL = "https://api.openweathermap.org/data/2.5/forecast?";
+    const queries = {
+      appid: "9f2cf3ec45c5d33adb0230c3659633ed",
+      lang: "pt_br",
+      units: "metric",
+      lat: "-23.7242643",
+      lon: "-47.2745313",
+    };
+
+    const url = baseURL.concat(new URLSearchParams(queries).toString());
+
+    (async () => {
+      try {
+        const { data } = await axios.get(url);
+
+        const maximumAPIdate = data?.list[data?.list?.length - 2];
+
+        const forecast = data?.list?.filter(
+          (item: any) =>
+            moment(eventDate).format("L") ===
+              moment(item?.dt_txt).format("L") ||
+            moment(maximumAPIdate?.dt_txt).format("L") ===
+              moment(item?.dt_txt).format("L")
+        );
+
+        setForecast({ ...data, list: forecast });
+      } catch (err) {
+        console.error(`Ocorreu um erro ao consultar previsão do tempo: ${err}`);
+      }
+    })();
+  }, [eventDate]);
+
+  if (!forecast?.list?.length) {
+    return null;
+  }
+
+  const { city, list } = forecast;
+
+  return (
+    <div className={styles.wrapper}>
+      <h5 className={styles.title}>
+        Previsão para o dia {moment(list[0]?.dt_txt).format("DD")} em{" "}
+        {city?.name}
+      </h5>
+
+      <div className={styles.forecast}>
+        {list?.map((item: any) => {
+          const { dt, dt_txt, weather, main } = item;
+
+          const hour = moment(dt_txt).get("h");
+          const weatherId = weather[0]?.id?.toString();
+          const weatherDescription = weather[0]?.description;
+          const temp = main?.temp_max;
+
+          if (![9, 15, 18].includes(hour)) {
+            return null;
+          }
+
+          const morning = hour === 9;
+          const afternoon = hour === 15;
+          const night = hour === 18;
+
+          return (
+            <div key={dt} className={styles.weather}>
+              {morning && (
+                <div className={styles.block}>
+                  {getIcon(weatherId)}
+                  <h6 className={styles.period}>Manhã</h6>
+                  <p className={styles.celsius}>{Math.round(temp)}°C</p>
+                  <small className={styles.description}>
+                    {weatherDescription}
+                  </small>
+                </div>
+              )}
+              {afternoon && (
+                <div className={styles.block}>
+                  {getIcon(weatherId)}
+                  <h6 className={styles.period}>Tarde</h6>
+                  <p className={styles.celsius}>{Math.round(temp)}°C</p>
+                  <small className={styles.description}>
+                    {weatherDescription}
+                  </small>
+                </div>
+              )}
+              {night && (
+                <div className={styles.block}>
+                  {getIcon(weatherId)}
+                  <h6 className={styles.period}>Noite</h6>
+                  <p className={styles.celsius}>{Math.round(temp)}°C</p>
+                  <small className={styles.description}>
+                    {weatherDescription}
+                  </small>
+                </div>
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Forecast;
