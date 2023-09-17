3aa8d92e1f14dc888cb98220964ee045bf300f98
David <davidsilva.sp@outlook.com>
2023-09-17 14:38:05 -0300

    feat pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
+const withPWA = require('next-pwa')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+})(nextConfig)
