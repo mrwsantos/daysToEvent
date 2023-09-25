ab6796ef85c59e5e446174d9c0122ff2f02fe2c9
David <davidsilva.sp@outlook.com>
2023-09-24 23:42:41 -0300

    test fs/promises

--- src/pages/api/reset/index.ts
+++ src/pages/api/reset/index.ts
@@ -1,11 +1,14 @@
+import fsPromises from "node:fs/promises";
+
 import { NextApiRequest, NextApiResponse } from "next";
-// import { registrationsFilePath, saveRegistrations } from "../register";
+import { registrationsFilePath } from "../register";
 
 export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
   if (request.method == "POST") {
+    await fsPromises.writeFile(registrationsFilePath, [])
     // saveRegistrations(registrationsFilePath, []);
 
     return response.status(204).end();
