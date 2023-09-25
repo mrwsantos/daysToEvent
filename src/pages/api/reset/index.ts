8dc3e3dbbe58401622b7f12883535834115c4505
David <davidsilva.sp@outlook.com>
2023-09-24 23:55:11 -0300

    test

--- src/pages/api/reset/index.ts
+++ src/pages/api/reset/index.ts
@@ -2,16 +2,16 @@ import fsPromises from "node:fs/promises";
 import path from 'node:path';
 
 import { NextApiRequest, NextApiResponse } from "next";
-// import { registrationsFilePath } from "../register";
+import { registrationsFilePath } from "../register";
 
-const registrationsFilePath = path.join(process.cwd(), 'database/userData.json')
+// const registrationsFilePath = path.join(process.cwd(), 'database/userData.json')
 
 export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
   if (request.method == "PUT") {
-    await fsPromises.writeFile(registrationsFilePath, [])
+    await fsPromises.writeFile(registrationsFilePath, JSON.stringify([]))
     // saveRegistrations(registrationsFilePath, []);
 
     return response.status(204).end();
